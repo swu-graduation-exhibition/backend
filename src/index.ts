6921e6ad7475007bedc448fb860a08407ed87f18
dragontaek-lee <dragontaek98@naver.com>
2023-09-03 22:49:17 +0900

    feat: add error handler

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
 import cors from "cors";
-// import errorHandler from "./middlewares/errorHandler";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 const PORT = 3000;
 
 app.use(
-  cors({
-    credentials: true,
-  })
+    cors({
+        credentials: true,
+    })
 );
 app.use(express.json());
 
 app.use("/", router);
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("server running");
+    res.send("server running");
 });
 
 app.listen(PORT, () => {
-  console.log("hello swu exhibition");
+    console.log("hello swu exhibition");
 });
 
-//app.use(errorHandler);
+app.use(errorHandler);
 
 export default app;
