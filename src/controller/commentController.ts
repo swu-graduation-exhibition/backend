1c5f6b094fae47678a3ff469b6f3b99ad66da0d5
dragontaek-lee <dragontaek98@naver.com>
2023-09-13 23:04:47 +0900

    feat: comment controller

---
+++ src/controller/commentController.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+import { SwuIdException } from "../models/SwuException";
+import { rm, sc } from "../constants";
+import { fail, success } from "../constants/response";
+import { commentService } from "../service";
+
+const designer = async (req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return next(new SwuIdException(sc.BAD_REQUEST, false, rm.BAD_REQUEST));
+    }
+
+    let id = req.query.id as string;
+    let page = req.query.page as string;
+
+    if (!page) {
+        return next(new SwuIdException(sc.BAD_REQUEST, false, rm.BAD_REQUEST));
+    }
+
+    if (!id) {
+        id = "";
+    }
+
+    try {
+        const commentList = await commentService.getDesignerCommentList(id, +page);
+        return res.status(sc.OK).send(success(sc.OK, rm.GET_PROJECT_LIST_SUCCESS, commentList));
+    } catch (e) {
+        return next(e);
+    }
+};
+
+const project = async (req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return next(new SwuIdException(sc.BAD_REQUEST, false, rm.BAD_REQUEST));
+    }
+
+    const { id, page } = req.query;
+
+    if (!id || !page) {
+        return next(new SwuIdException(sc.BAD_REQUEST, false, rm.BAD_REQUEST));
+    }
+
+    try {
+        const commentList = await commentService.getProjectCommentList(+id, +page);
+        return res.status(sc.OK).send(success(sc.OK, rm.GET_PROJECT_SUCCESS, commentList));
+    } catch (e) {
+        return next(e);
+    }
+};
+
+const commentController = {
+    designer,
+    project,
+};
+
+export default commentController;
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,2 +1,3 @@
 export { default as projectController } from "./projectController";
 export { default as designerController } from "./designerController";
+export { default as commentController } from "./commentController";
