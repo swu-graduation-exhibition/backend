90749a0b647454b15953a0533cb43e3bf2491aa2
dragontaek-lee <dragontaek98@naver.com>
2023-09-14 00:58:21 +0900

    feat: add comment message

--- src/constants/responseMessage.ts
+++ src/constants/responseMessage.ts
@@ -8,4 +8,9 @@ export default {
     GET_PROJECT_SUCCESS: "프로젝트 가져오기 성공",
     GET_DESIGNER_LIST_SUCCESS: "디자이너 목록 가져오기 성공",
     GET_DESIGNER_SUCCESS: "디자이너 목록 가져오기 성공",
+
+    GET_DESIGNER_COMMENT_LIST_SUCCESS: "디자이너 메세지 목록 가져오기 성공",
+    GET_PROJECT_COMMENT_LIST_SUCCESS: "프로젝트 메세지 목록 가져오기 성공",
+    CREATE_DESIGNER_COMMENT_SUCCESS: "디자이너 메세지 보내기 성공",
+    CREATE_PROJECT_COMMENT_SUCCESS: "프로젝트 메세지 보내기 성공",
 };
